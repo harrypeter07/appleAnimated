4261c8f89a927f3c48d3cbc54b125b2f9b202bbc
harrypeter07 <hassanmansuri570@gmail.com>
2024-12-10 14:40:14 +0530

    second commit

---
+++ src/main.jsx
@@ -0,0 +1,66 @@
+// Sentry.init({
+//   dsn: "https://d1460ce9672f086d43706610d32645e2@o4508405761179648.ingest.us.sentry.io/4508405763473408",
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+
+// import * as Sentry from "@sentry/react";
+
+//   integrations: [
+//     Sentry.browserTracingIntegration(),
+//     Sentry.metrics.metricsAggregatorIntegration(),
+//     Sentry.reactRouterV6BrowserTracingIntegration({
+//       useEffect: React.useEffect,     
+//     }),
+
+//     Sentry.replayIntegration(),
+//   ],
+//   // Tracing
+//   tracesSampleRate: 1.0, //  Capture 100% of the transactions
+//   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+//   tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+//   // Session Replay
+//   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+//   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+// });
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://d1460ce9672f086d43706610d32645e2@o4508405761179648.ingest.us.sentry.io/4508405763473408",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
