7f9a014db55b1387ab48db8c6d2d75f386a7a3f4
Hassan Mansuri <146269350+harrypeter07@users.noreply.github.com>
2024-12-10 17:11:01 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -38,26 +38,7 @@ import App from './App.jsx'
 import './index.css'
 
 //...
-import * as Sentry from "@sentry/react";
 
-Sentry.init({
-  dsn: "https://d1460ce9672f086d43706610d32645e2@o4508405761179648.ingest.us.sentry.io/4508405763473408",
-  integrations: [
-    Sentry.browserTracingIntegration(),
-   // Sentry.metrics.metricsAggregatorIntegration(),
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-    }),
-    Sentry.replayIntegration({
-      maskAllText: false,
-      blockAllMedia: false,
-    }),
-  ],
-  tracesSampleRate: 1.0, 
-  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0, 
-});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
