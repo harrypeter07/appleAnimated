34c4b2ec0232fddc8ecb0603cd7593fb631d3b60
Hassan Mansuri <hassanmansuri570@gmail.com>
2025-08-12 22:57:23 +0530

    Remove Sentry dependencies and related plugin configuration from package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "student-jx5",
-    project: "javascript-react"
-  }), sentryVitePlugin({
-    org: "student-jx5",
-    project: "javascript-react"
-  })],
+  plugins: [react()],
 
   build: {
     sourcemap: true
